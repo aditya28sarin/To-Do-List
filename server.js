8c4d090aa11967d37ef76e7cddab2f4d537dd23c
Aditya Sarin <aditya28sarin@gmail.com>
2020-11-30 10:30:21 +0530

    Added Procfile and gitignore file

--- server.js
+++ server.js
@@ -142,6 +142,12 @@ app.post('/delete', function(req,res){
 	
 });
 
-app.listen(3000,function(){
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port || ,function(){
     console.log('Server is listening at Port 3000..')
 });
